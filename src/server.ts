e79045e3a0e02f88b7809483db92e95b903f84ec
Vinicius Borges <viniciusborges23@gmail.com>
2020-10-13 23:00:02 -0300

    Makes a simple request

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import axios from 'axios';
 
 const { PORT } = process.env;
 
 const app = express();
 
-app.get('/', (_, res) => {
-  res.json({ ok: true });
+app.get('/recipes', async (req: Request, res: Response) => {
+  const recipes = await axios(`http://www.recipepuppy.com/api/?i=${req.query.i}`);
+
+  res.json(recipes.data);
 });
+
+app.get('*', (_, res) => res.sendStatus(405));
+app.put('*', (_, res) => res.sendStatus(405));
+app.delete('*', (_, res) => res.sendStatus(405));
+app.post('*', (_, res) => res.sendStatus(405));
 
 app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
