2368e20a908d4d6146172e32a81c8478699aaafb
Vinicius Borges <vinicius.borges@moblee.net>
2020-10-15 21:58:02 -0300

    Make giphy request and validade params

--- src/controllers/RecipesController.ts
+++ src/controllers/RecipesController.ts
@@ -1,8 +1,33 @@
+import pMap from 'p-map';
 import { Router, Request, Response } from 'express';
 import recipePuppyApi from '../services/recipePuppyApi';
 import giphyApi from '../services/giphyApi';
 
 const { GIPHY_API_KEY } = process.env;
+
+function sanitize(title: string) {
+  return title.trim().replace(/\n|\r|\t/g, '');
+}
+
+async function getGIF(title: string) {
+  let gif = '';
+
+  try {
+    const { data: { data } } = await giphyApi.get(`search?api_key=${GIPHY_API_KEY}&q=${title}&limit=1&lang=en`);
+
+    gif = data[0]?.url || '';
+  } catch (e) {
+    // noop
+  }
+
+  return gif;
+}
+
+interface PuppyRecipes {
+  title: string;
+  ingredients: string;
+  href: string;
+}
 
 class RecipesController {
   public path = '/recipes';
@@ -14,26 +39,52 @@ class RecipesController {
   }
 
   public routes() {
-    this.router.get(this.path, this.getAll);
+    this.router.get(this.path, RecipesController.getAll);
   }
 
-  getAll = async (req: Request, res: Response) => {
-    console.log(req.query.i);
-    // validate params
-    try {
-      const { data: recipesData } = await recipePuppyApi.get(`?i=${req.query.i}`);
+  static validateIngredients(ingredients: string) {
+    if (!ingredients) {
+      throw new Error('Ingredients missing.');
+    }
 
-      if (!recipesData.results) {
-        res.json([]);
+    if (ingredients.split(',').length > 3) {
+      throw new Error('Max ingredients allowed.');
+    }
+  }
+
+  static async getAll(req: Request, res: Response): Promise<void> {
+    try {
+      const ingredientsQuery = req.query.i as string;
+
+      RecipesController.validateIngredients(ingredientsQuery);
+
+      const { data: { results } } = await recipePuppyApi.get(`?i=${ingredientsQuery}`);
+
+      if (!results) {
+        res.json({
+          keywords: ingredientsQuery.split(','),
+          recipes: [],
+        });
         return;
       }
 
-      const giphy = await giphyApi.get(`search?api_key=${GIPHY_API_KEY}&q=${recipesData.results[0].title}&limit=1&lang=en`);
+      const mapper = async ({ title, ingredients, href: link }: PuppyRecipes) => {
+        const gif = await getGIF(title);
 
-      console.log('recipes', recipesData);
-      console.log('giphy', giphy.data.data[0]?.url);
+        return {
+          title: sanitize(title),
+          ingredients: ingredients.split(', ').sort(),
+          link,
+          gif,
+        };
+      };
 
-      res.json(recipesData.results);
+      const recipes = await pMap(results, mapper);
+
+      res.json({
+        keywords: ingredientsQuery.split(','),
+        recipes,
+      });
     } catch (e) {
       console.log(e);
       res.sendStatus(500);
